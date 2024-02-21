1bc85a05cace51d44911342fcc50b61bb526ce48
sapmazum <sapmazum@C-PF43J4H7.apac.nsn-net.net>
2024-02-21 12:45:25 +0530

    First Commit

---
+++ DSA/src/com/DSA/Arrays.java
@@ -0,0 +1,26 @@
+package com.DSA;
+
+import java.util.Scanner;
+
+public class Arrays {
+
+	public static void main(String[] args) {
+		
+		System.out.println("enter");
+		
+		Scanner s= new Scanner(System.in);
+		
+		int a=s.nextInt();
+		
+		int abc[]= new int[3];
+		
+		for(int i=0;i<3;i++) {
+			abc[i]=a;
+			System.out.println(abc[i]);// added commit
+		}
+		
+		
+
+	}
+
+}
